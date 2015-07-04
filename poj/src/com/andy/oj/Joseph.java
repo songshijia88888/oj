d942e6fad80c6bf74f3b1594ff2cb706ae76f08b
Andy <Andy@Andy-PC.mshome.net>
2015-07-04 11:15:19 +0800

    Joseph, problem numbered 1012 on POJ, is solved. Acceepted without any doubt. Problem do can get solved only  with the length information, cool!

--- poj/src/com/andy/oj/Joseph.java
+++ poj/src/com/andy/oj/Joseph.java
@@ -4,47 +4,39 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
 
 public class Joseph {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		List<Integer> listsrc = new LinkedList<Integer>();
-		List<Integer> listtemp = new LinkedList<Integer>();
 		Map<Integer, Integer> mapresult = new HashMap<Integer, Integer>();
 		StringBuffer sb = new StringBuffer();
+		
 		String s = br.readLine();
 		int k = 0;
 		int m = 0;
 		int index = 0;
-		while(!"0".equals(s)){
-			listsrc.clear();
-			listtemp.clear();
+		while (!"0".equals(s)) {
 			k = Integer.parseInt(s);
-			if(mapresult.containsKey(k)){
+			if (mapresult.containsKey(k)) {
 				sb.append(mapresult.get(k)).append("\n");
 				s = br.readLine();
 				continue;
 			}
-			for(int i = 0; i < 2 * k; i++){
-				listsrc.add(i);
-			}
 			
-			listtemp.addAll(listsrc);
-
 			m = k;
 			index = 0;
-			while (listtemp.size() > k) {
-				index = (m + index) % listtemp.size();
+			int size = 2 * k;
+			int varysize = 2 * k;
+			
+			while (varysize > k) {
+				index = (m + index) % varysize;
 				if (index > k - 1) {
-					listtemp.remove(index);
+					varysize--;
 				} else {
 					m++;
 					index = 0;
-					listtemp.clear();
-					listtemp.addAll(listsrc);
+					varysize = size;
 				}
 			}
 			sb.append(m + 1).append("\n");
