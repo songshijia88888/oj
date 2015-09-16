8d3a518c437378cc97384252420d33786b2f2fdc
Andy <songshijia88888@hotmail.com>
2015-09-17 02:16:44 +0800

    still need to deal input like 2 4...

--- poj/src/com/andy/oj/NumberSequence2.java
+++ poj/src/com/andy/oj/NumberSequence2.java
@@ -1,55 +1,47 @@
 package com.andy.oj;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.math.BigInteger;
 
-public class NumberSequence {
-	public static void main(String[] args){
-		System.out.println(getDigitsOfssk(999));
-		int k = 1;
-		StringBuffer sk = new StringBuffer();
-		sk.append(k);
-		StringBuffer ssk = new StringBuffer();
-		ssk.append(sk.toString());
-		while(k < 999){
-			k++;
-			sk.append(k);
-			ssk.append(sk.toString());
+public class NumberSequence2 {
+	// 解法说明：
+	// 1.用方法getDigitsOfssk(k)找到长度刚刚小于目标值的ssk.
+	// 2.将k值拿到后，计算ssk的长度比目标值小的量m.
+	// 3.用方法getDigitsOfsk(k1)找到长度刚刚小于m的sk1.
+	// 4.将k1值拿到后，计算sk1的长度比m小的量n.
+	// 5.整数k1 + 1中，第n个数字就是结果！
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+		int cases = Integer.parseInt(s);
+		int count = 0;
+		StringBuffer sb = new StringBuffer();
+		while(count < cases){
+			s = br.readLine();
+			count++;
+			
+			BigInteger target = new BigInteger(s);
+			int i = 1;
+			while(getDigitsOfssk(i).compareTo(target) < 0){
+				i = i + 1;
+			}
+			long target2 = target.subtract(getDigitsOfssk(i - 1)).longValue();
+			
+			int i2 = 1;
+			while(getDigitsOfsk(i2) < target2){
+				i2 = i2 + 1;
+			}
+			int n = (int)(target2 - getDigitsOfsk(i2 - 1));
+			String k1_1  = String.valueOf(i2);
+			
+			sb.append(k1_1.charAt(n - 1));
+			sb.append("\n");
 		}
 		
-		System.out.println(ssk.toString().length());
-		
-		
-		//the digit at 2147483647
-		BigInteger target = new BigInteger("2147483647");
-		int i = 1;
-		while(getDigitsOfssk(i).compareTo(target) < 0){
-			i = i + 1;
-		}
-		System.out.println(i);
-		
-		System.out.println(getDigitsOfssk(31267));
-		System.out.println("2147483647");
-		System.out.println(getDigitsOfssk(31268));
-		
-		//the digit at 105170
-		int i2 = 1;
-		while(getDigitsOfsk(i2) < 105170){
-			i2 = i2 + 1;
-		}
-		System.out.println(i2);
-		
-		System.out.println(getDigitsOfsk(23255));
-		System.out.println(105170);
-		System.out.println(getDigitsOfsk(23256));
-		
-		//the result is 2.
-		//解法说明：
-		//1.用方法getDigitsOfssk(k)找到长度刚刚小于目标值的ssk.
-		//2.将k值拿到后，计算ssk的长度比目标值小的量m.
-		//3.用方法getDigitsOfsk(k1)找到长度刚刚小于m的sk1.
-		//4.将k1值拿到后，计算sk1的长度比m小的量n.
-		//5.整数k1 + 1中，第n个数字就是结果！
-		
+		System.out.println(sb.toString());
 	}
 	
 	
